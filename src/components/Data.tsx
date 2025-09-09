6967bebf5b1fc845935570c423d08107dd27254c
juheedhone <juheedhone12@gmail.com>
2025-09-09 12:35:07 +0530

    animation while fetching data.

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -10,7 +10,55 @@ interface Props {
 
 const Data = ({ result, loading }: Props) => {
   if (loading) {
-    return <div className="w-full h-5 bg-gray-400 animate-pulse"></div>;
+    return (
+      <>
+        <div className="w-full p-4 mx-auto rounded-md">
+          <div className="flex space-x-4 animate-pulse">
+            <div className="bg-gray-200 rounded-full size-10"></div>
+            <div className="flex-1 py-1 space-y-6">
+              <div className="h-2 bg-gray-200 rounded"></div>
+              <div className="space-y-3">
+                <div className="grid grid-cols-3 gap-4">
+                  <div className="h-2 col-span-2 bg-gray-200 rounded"></div>
+                  <div className="h-2 col-span-1 bg-gray-200 rounded"></div>
+                </div>
+                <div className="h-2 bg-gray-200 rounded"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full p-4 mx-auto rounded-md">
+          <div className="flex space-x-4 animate-pulse">
+            <div className="bg-gray-200 rounded-full size-10"></div>
+            <div className="flex-1 py-1 space-y-6">
+              <div className="h-2 bg-gray-200 rounded"></div>
+              <div className="space-y-3">
+                <div className="grid grid-cols-3 gap-4">
+                  <div className="h-2 col-span-2 bg-gray-200 rounded"></div>
+                  <div className="h-2 col-span-1 bg-gray-200 rounded"></div>
+                </div>
+                <div className="h-2 bg-gray-200 rounded"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full p-4 mx-auto rounded-md">
+          <div className="flex space-x-4 animate-pulse">
+            <div className="bg-gray-200 rounded-full size-10"></div>
+            <div className="flex-1 py-1 space-y-6">
+              <div className="h-2 bg-gray-200 rounded"></div>
+              <div className="space-y-3">
+                <div className="grid grid-cols-3 gap-4">
+                  <div className="h-2 col-span-2 bg-gray-200 rounded"></div>
+                  <div className="h-2 col-span-1 bg-gray-200 rounded"></div>
+                </div>
+                <div className="h-2 bg-gray-200 rounded"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
   }
 
   return (
